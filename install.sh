cbf51143bbc9ba772b3e75c43f89aea0da947866
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-08-01 06:29:31 -0600

    Fixed install errors

--- install.sh
+++ install.sh
@@ -8,19 +8,15 @@
 #
 
 sudo apt-get update
-
 sudo apt-get upgrade
-
-sudo apt-get install vim python-virtualenv gdal-bin geos libpq-dev postgresql-common python-dev postgresql-client postgresql postgresql-contrib postgis git openjdk-7-jre-headless unzip python-memcache libjpeg libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev supervisor nginx
+sudo apt-get install vim python-virtualenv gdal-bin libgeos-dev libpq-dev postgresql-common python-dev postgresql-client postgresql postgresql-contrib postgis git openjdk-7-jre-headless unzip python-memcache libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev supervisor nginx
 
 #
 # Get the floodlight code
 #
 
 mkdir -p ~/www
-
 sudo mkdir -p ~/virt_env
-
 cd ~/www
 
 git clone https://github.com/zmetcalf/storybase.git floodlight
@@ -31,21 +27,13 @@ git clone https://github.com/zmetcalf/storybase.git floodlight
 #
 
 cd ~/www/floodlight
-
 git fetch
-
 git checkout zm_local_install
-
 virtualenv ~/virt_env/storybase
-
 source ~/virt_env/storybase/bin/activate
-
 pip install -r requirements.txt
-
 pip install gunicorn
-
 cd ~/www
-
 git clone https://github.com/zmetcalf/storybase_solr.git
 
 #
@@ -53,15 +41,10 @@ git clone https://github.com/zmetcalf/storybase_solr.git
 #
 
 sudo su - postgres
-
 createuser --superuser floodlight
-
 psql
-
 \password floodlight # floodlight
-
 psql mydatabasename -c "CREATE EXTENSION postgis";
-
 
 sudo -u postgres createdb floodlight
 
@@ -74,7 +57,6 @@ sudo psql floodlight -c "CREATE EXTENSION postgis";
 #
 
 sudo /etc/init.d/postgresql restart
-
 sudo su - postgres
 
 
@@ -83,15 +65,11 @@ sudo su - postgres
 #
 
 cp gunicorn.conf.py /home/admin/www/floodlight
-
 sudo cp supervisor/floodlight.conf /etc/supervisor/conf.d
-
 sudo cp supervisor/floodlight_solr.conf /etc/supervisor/conf.d
-
 sudo cp nginx/floodlight /etc/nginx/sites-available
 
 sudo ln -s /etc/nginx/sites-available/floodlight /etc/nginx/sites-enabled/floodlight
 
 sudo service nginx restart
-
 sudo service supervisor restart
